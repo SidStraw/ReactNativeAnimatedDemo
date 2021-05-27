a028009036494af1892d9b15c472ee61670f1418
Sid-Straw <j80304xz@gmail.com>
2021-05-28 01:27:26 +0800

    feat: set Animated.ScrollView's Animated.event

--- App.tsx
+++ App.tsx
@@ -1,53 +1,36 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {useRef} from 'react';
-import {Animated, Button, View, SafeAreaView, Dimensions} from 'react-native';
+import {
+  Animated,
+  Button,
+  View,
+  SafeAreaView,
+  Dimensions,
+  Text,
+} from 'react-native';
 const {width, height} = Dimensions.get('window');
 
 const App = () => {
   const sidebarAnimationValue = useRef(new Animated.Value(0)).current;
-  const scaleValue = useRef(0);
 
   const buttonAnimationValue = React.useRef(
     [...Array(10)].map(() => new Animated.Value(0)),
   ).current;
 
-  const runAnimationOnClick = () => {
-    buttonAnimationValue.forEach(animated => animated.setValue(0));
-    scaleValue.current = +!scaleValue.current;
-
-    Animated.timing(sidebarAnimationValue, {
-      toValue: scaleValue.current,
-      useNativeDriver: true,
-    }).start(({finished}) => {
-      if (finished) {
-        const animatedConfig = {
-          toValue: 1,
-          useNativeDriver: true,
-        };
-        Animated.stagger(
-          300,
-          buttonAnimationValue.map(animated =>
-            Animated.timing(animated, animatedConfig),
-          ),
-        ).start();
-      }
-    });
-  };
-
   return (
     <SafeAreaView>
       <View style={{width, height}}>
-        <Button title="滑入 / 滑出" onPress={runAnimationOnClick} />
         <Animated.View
           style={{
             flex: 1,
             width: 200,
             backgroundColor: '#427685',
+            overflow: 'hidden',
             transform: [
               {
                 translateX: sidebarAnimationValue.interpolate({
-                  inputRange: [0, 1],
-                  outputRange: [-200, 0],
+                  inputRange: [0, 2000],
+                  outputRange: [-200, 100],
                 }),
               },
             ],
@@ -69,6 +52,68 @@ const App = () => {
             </Animated.View>
           ))}
         </Animated.View>
+        <View style={{flex: 4}}>
+          <Animated.ScrollView
+            overScrollMode={'never'}
+            style={{flex: 1, paddingTop: 100}}
+            scrollEventThrottle={30}
+            onScroll={Animated.event(
+              [
+                {
+                  nativeEvent: {contentOffset: {y: sidebarAnimationValue}},
+                },
+              ],
+              {
+                listener: event => {
+                  console.log(event);
+                },
+                useNativeDriver: false,
+              },
+            )}>
+            <Animated.Text
+              style={[
+                {
+                  position: 'absolute',
+                  zIndex: 100,
+                  textAlign: 'center',
+                  top: sidebarAnimationValue.interpolate({
+                    inputRange: [0, 300],
+                    outputRange: [30, 0],
+                  }),
+                  left: 0,
+                  right: 0,
+                  padding: 30,
+                  backgroundColor: '#9EBEC6',
+                  opacity: sidebarAnimationValue.interpolate({
+                    inputRange: [0, 150],
+                    outputRange: [1, 0],
+                  }),
+                },
+              ]}>
+              <Text>DEMO Title</Text>
+            </Animated.Text>
+
+            <Animated.View
+              style={{
+                marginTop: 50,
+                transform: [
+                  {
+                    translateY: sidebarAnimationValue.interpolate({
+                      inputRange: [0, 250],
+                      outputRange: [100, 0],
+                      extrapolate: 'clamp',
+                    }),
+                  },
+                ],
+              }}>
+              {[...Array(100)].map((val, i) => (
+                <Text key={i} style={{textAlign: 'center', padding: 10}}>
+                  DEMO {i + 1}
+                </Text>
+              ))}
+            </Animated.View>
+          </Animated.ScrollView>
+        </View>
       </View>
     </SafeAreaView>
   );
